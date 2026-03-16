dd3961434db680c71a6d082415283e6eed883826
Leonardo Shibata <shibataleo2606@gmail.com>
2026-03-16 01:12:19 -0300

    Add bin-to-gray Quartus project and VHDL
    Add a new Quartus project for a 4-bit binary-to-Gray converter (Conv_bin_gray): project files (.qpf, .qsf) and Conv_bin_gray.vhd implementing combinational Gray conversion. Add Aula_01_ex_01_base.qsf with pin assignments for exercise 1, add a .gitignore to exclude Quartus/Logisim build artifacts, and relocate the Logisim circuit file into Aula_01/Exercicio_02/Logisim for consistent project structure.

---
+++ Aula_01/Exercicio_02/Quartus/Conv_bin_gray.vhd
@@ -0,0 +1,39 @@
+--------------------------------------------------------------------------------
+-- Project :
+-- File    :
+-- Autor   :
+-- Date    :
+--
+--------------------------------------------------------------------------------
+-- Description :
+--
+--------------------------------------------------------------------------------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY Conv_bin_gray IS
+  PORT (
+  ------------------------------------------------------------------------------
+  --Insert input ports below
+    bin_i        : IN  std_logic_vector(3 DOWNTO 0); -- input vector example
+  ------------------------------------------------------------------------------
+  --Insert output ports below
+    gray_o        : OUT std_logic_vector(3 DOWNTO 0)  -- output vector example
+    );
+END Conv_bin_gray;
+
+--------------------------------------------------------------------------------
+--Complete your VHDL description below
+--------------------------------------------------------------------------------
+
+ARCHITECTURE TypeArchitecture OF Conv_bin_gray IS
+
+BEGIN
+
+gray_o(3) <= bin_i(3);
+gray_o(2) <= bin_i(3) XOR bin_i(2);
+gray_o(1) <= bin_i(2) XOR bin_i(1);
+gray_o(0) <= bin_i(1) XOR bin_i(0);
+
+END TypeArchitecture;
