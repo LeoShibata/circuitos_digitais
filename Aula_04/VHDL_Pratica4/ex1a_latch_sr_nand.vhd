ae664780736959daaf45c1efd191114fff08b73c
Sergio Roncato Maccari <sergio.r.maccari@gmail.com>
2026-04-24 20:59:38 -0300

    testa ai

--- Aula_04/VHDL_Pratica4/ex1a_latch_sr_nand.vhd
+++ Aula_04/VHDL_Pratica4/ex1a_latch_sr_nand.vhd
@@ -1,8 +1,10 @@
--- Exercicio 1a) Latch SR com portas NAND
--- Entradas ativas em baixo (S_n, R_n)
+-- Exercicio 1a) Latch SR estilo NAND (entradas S_n e R_n ativas em BAIXO)
+-- Versao comportamental (sem realimentacao combinacional explicita)
+-- para evitar erro de "combinational loop" no Quartus.
+--
 -- Tabela verdade:
 --   S_n R_n |  Q   QN
---    0   0  |  1   1   (proibido)
+--    0   0  |  1   1   (proibido - evitar)
 --    0   1  |  1   0   (set)
 --    1   0  |  0   1   (reset)
 --    1   1  |  Q   QN  (memoria)
@@ -18,10 +20,25 @@ entity latch_sr_nand is
 end latch_sr_nand;
 
 architecture latch_sr_nand_arch of latch_sr_nand is
-  signal q_i, qn_i : std_logic;
+  signal q_i  : std_logic;
+  signal qn_i : std_logic;
 begin
-  q_i  <= S_n nand qn_i;
-  qn_i <= R_n nand q_i;
-  Q    <= q_i;
-  QN   <= qn_i;
+  process(S_n, R_n)
+  begin
+    if S_n = '0' and R_n = '0' then
+      -- estado proibido: replica o que o NAND fisico faria (ambos em 1)
+      q_i  <= '1';
+      qn_i <= '1';
+    elsif S_n = '0' and R_n = '1' then
+      q_i  <= '1';
+      qn_i <= '0';
+    elsif S_n = '1' and R_n = '0' then
+      q_i  <= '0';
+      qn_i <= '1';
+    end if;
+    -- caso S_n=R_n=1: nada eh atribuido => Quartus infere LATCH (memoria)
+  end process;
+
+  Q  <= q_i;
+  QN <= qn_i;
 end latch_sr_nand_arch;
--- Aula_04/VHDL_Pratica4/ex1b_latch_sr_nor.vhd
+++ Aula_04/VHDL_Pratica4/ex1b_latch_sr_nor.vhd
@@ -1,11 +1,17 @@
--- Exercicio 1b) Latch SR com portas NOR
--- Entradas ativas em alto (S, R)
+-- Exercicio 1b) Latch SR estilo NOR (entradas S e R ativas em ALTO)
+-- Versao comportamental (sem realimentacao combinacional explicita)
+-- para evitar erro de "combinational loop" no Quartus.
+--
 -- Tabela verdade:
---    S   R  |  Q   QN
---    0   0  |  Q   QN  (memoria)
---    0   1  |  0   1   (reset)
---    1   0  |  1   0   (set)
---    1   1  |  0   0   (proibido)
+--   S   R |  Q   QN
+--   0   0 |  Q   QN  (memoria)
+--   0   1 |  0   1   (reset)
+--   1   0 |  1   0   (set)
+--   1   1 |  0   0   (proibido)
+--
+-- Atencao: na placa do laboratorio os botoes estao em '1' em repouso.
+-- Para usar este latch direto com os botoes, INVERTA os sinais antes
+-- (ou troque pelo latch NAND, que ja eh ativo em baixo).
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -18,10 +24,25 @@ entity latch_sr_nor is
 end latch_sr_nor;
 
 architecture latch_sr_nor_arch of latch_sr_nor is
-  signal q_i, qn_i : std_logic;
+  signal q_i  : std_logic;
+  signal qn_i : std_logic;
 begin
-  q_i  <= R nor qn_i;
-  qn_i <= S nor q_i;
-  Q    <= q_i;
-  QN   <= qn_i;
+  process(S, R)
+  begin
+    if S = '1' and R = '1' then
+      -- estado proibido: replica o que o NOR fisico faria (ambos em 0)
+      q_i  <= '0';
+      qn_i <= '0';
+    elsif S = '1' and R = '0' then
+      q_i  <= '1';
+      qn_i <= '0';
+    elsif S = '0' and R = '1' then
+      q_i  <= '0';
+      qn_i <= '1';
+    end if;
+    -- caso S=R=0: nada eh atribuido => Quartus infere LATCH (memoria)
+  end process;
+
+  Q  <= q_i;
+  QN <= qn_i;
 end latch_sr_nor_arch;
